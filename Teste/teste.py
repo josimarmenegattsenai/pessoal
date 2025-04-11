7fc3489a82b8106895cf0b479af8fdc4795e8e32
josimarmenegattsenai <josimar.menegatt@edu.sc.senai.br>
2025-04-11 15:05:41 -0300

    Update teste.py

--- Teste/teste.py
+++ Teste/teste.py
@@ -1,1 +1,19 @@
-print ("Ola Mundo")
+import webbrowser
+
+html_content = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exemplo Simples</title>
+</head>
+<body>
+    <h1>Olá, mundo!</h1>
+    <p>Este HTML foi gerado por Python.</p>
+</body>
+</html>
+"""
+
+with open("pagina.html", "w", encoding="utf-8") as arquivo:
+    arquivo.write(html_content)
+
+webbrowser.open("pagina.html")
